75528d56ed2650c6d811ea74f0c74a275c5a0c90
asifilbal01890 <asifikbal01890@gmail.com>
2023-11-07 20:39:17 +0600

    home page complate

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,28 +1,70 @@
 import Image from "next/image";
+import { BsFacebook, BsThreeDots } from 'react-icons/bs';
+import { FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import { PiStar, PiStarFill } from 'react-icons/pi';
 import ImgOne from '../../../public/assets/homePageImage/Thumbnail-01.png';
 import ImgTwo from '../../../public/assets/homePageImage/Thumbnail-02.png';
 const Home = () => {
     return (
-        <div className="flex mt-[15px] ml-4">
-            <div>
-                <div></div>
-            </div>
-            <div className="flex items-start justify-between">
-                <div>
-                    <h1>Health benefits of an avocado</h1>
-                    <h3>Supports eye health:</h3>
-                    <p>Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.</p>
-                    <h3>Supports eye health: </h3>
-
+        <div className="flex mt-[15px] bg-white p-11 rounded-[10px]">
+            <div className="mr-[42px] mt-[60px]">
+                <div className="text-white bg-[#1877F2] rounded-full p-[6px]">
+                    <BsFacebook></BsFacebook>
                 </div>
-                <div>
+                <div className="text-white bg-[#2867B2] rounded-full mt-[22px] p-[6px]">
+                    <FaLinkedinIn></FaLinkedinIn>
+                </div>
+                <div className="text-white bg-[#1DA1F2] rounded-full mt-[22px] p-[6px]">
+                    <FaTwitter></FaTwitter>
+                </div>
+                <div className="text-white bg-[#E55527] rounded-full mt-[22px] p-[6px]">
+                    <BsThreeDots></BsThreeDots>
+                </div>
+            </div>
+            <div>
+            <div className="flex items-start gap-[30]">
+                <div className="w-[482px]">
+                    <h1 className="text-[38px] font-black leading-[49px]">Health Benefits Of An Avocado</h1>
+                    <h3 className="mt-[25px] text-lg font-bold leading-7">Supports eye health:</h3>
+                    <p className="mt-[10px] text-[15.1px] leading-6">Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.</p>
+                    <h3 className="mt-[25px] text-lg font-bold leading-7">Supports eye health: </h3>
+                    <p className="mt-[10px] text-[15.1px] leading-6">Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.</p>
+                </div>
+                <div className="!w-[520px] h-[320px]">
                 <Image
                     src={ImgOne}
                     alt="menuOneIcon"
-                    width="42.52"
-                    height="42.52"
+                    width="520"
+                    height="320"
+                    className="!w-[520px] h-[320px]"
                 />
                 </div>
+            </div>
+            <h3 className="mt-[42px] text-lg font-bold leading-7">Supports eye health: </h3>
+            <p className="text-[15.1px] mt-[10px] leading-6">Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. </p>
+            <div className="flex items-center justify-between gap-12 mt-[46px]">
+            <Image
+                    src={ImgTwo}
+                    alt="menuOneIcon"
+                    width="520"
+                    height="320"
+                    className="!w-[520px] h-[320px]"
+                />
+                <div>
+                    <h3 className="text-lg font-bold leading-7">Supports eye health:</h3>
+                    <p className="mt-[10px] text-[15px] leading-6">Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.. Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy</p>
+                </div>
+            </div>
+            <div className="mt-[62px]">
+                <h3 className="text-lg font-black text-[#212121]">Was This Helpful ?</h3>
+                <div className="mt-[10px] flex items-center gap-[10px]">
+                    <p className="text-[28px] text-[#E55527]"><PiStarFill></PiStarFill></p>
+                    <p className="text-[28px] text-[#E55527]"><PiStarFill></PiStarFill></p>
+                    <p className="text-[28px] text-[#E55527]"><PiStarFill></PiStarFill></p>
+                    <p className="text-[28px] text-[#E55527]"><PiStarFill></PiStarFill></p>
+                    <p className="text-[28px] text-[#E55527]"><PiStar></PiStar></p>
+                </div>
+            </div>
             </div>
         </div>
     );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,12 @@ import SideNavBar from '@/Components/NavBar/SideNavBar/SideNavBar'
 import TopNavBar from '@/Components/NavBar/TopNavBar/TopNavBar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className='bg-white'>
+  return <div className='bg-white text-[#212121]'>
     <div className='fixed z-50 w-full bg-white'>
     <TopNavBar></TopNavBar>
     </div>
@@ -13,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
    <SideNavBar></SideNavBar>
    </div>
    <div className='mt-[55px] ml-[55px] rounded-[240px] pt-[15px] pl-[15px] w-full'>
-    <Component {...pageProps} />
+    <Component className={inter.className} {...pageProps} />
    </div>
     </div>
   </div>
